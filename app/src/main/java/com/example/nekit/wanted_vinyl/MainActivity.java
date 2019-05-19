0c584c8188c668c42c7a94f3119c0c845f64b72d
nikita <myasov1992@list.ru>
2019-05-20 02:33:05 +0300

    it's done

--- app/src/main/java/com/example/nekit/wanted_vinyl/MainActivity.java
+++ app/src/main/java/com/example/nekit/wanted_vinyl/MainActivity.java
@@ -1,44 +1,41 @@
 package com.example.nekit.wanted_vinyl;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
     GoogleMap gMap;
-    final Place places = new Place();
-    private TextView tvWAYLF;
-    private EditText etFind;
-    private Button btFindThat;
-
+    Map<String, LatLng> places = new Place();
+    private SearchView loc;
+    private SupportMapFragment mapFragment;
     private String a, answerHTTP;
-
     private Gson gson = new GsonBuilder().create();
     private final String server = "https://www.google.com/";
     private Retrofit wantedvinyl = new Retrofit
@@ -49,45 +46,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private Finding find = wantedvinyl.create(Finding.class);
 
+    private static final double MARK_LATITUDE = 55.75222;
+    private static final double MARK_LONGTITUDE = 37.61556;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btFindThat = findViewById(R.id.FindThat);
-        etFind = findViewById(R.id.Find);
-        tvWAYLF = findViewById(R.id.WAYLF);
-        Check();
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        btFindThat.setOnClickListener(new View.OnClickListener() {
+        loc = findViewById(R.id.location);
+        checkMap();
+        createMapView();
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        loc.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View v){
-                a = tvWAYLF.getText().toString();
-
-                Call<String> call = find.performPostCall(places);
-
-                call.enqueue(new Callback<String>() {
-                    @Override
-                    public void onResponse(Call<String> call, Response<String> response) throws NullPointerException{
-                        Map<String, LatLng> map = gson.fromJson(response.body().toString(), TreeMap.class);
-                        answerHTTP = LatLng.NULL;
-                        tvWAYLF.setText(answerHTTP);
+            public boolean onQueryTextSubmit(String query) {
+                    String locationOfMyPrecious = loc.getQuery().toString();
+                    List<Address> addresses = null;
+                    if(locationOfMyPrecious!= null || !locationOfMyPrecious.equals("")){
+                        Geocoder geocoder = new Geocoder(MainActivity.this);
+                        try{
+                            addresses = geocoder.getFromLocationName(locationOfMyPrecious, 1);
+                        }catch(IOException e){
+                            e.printStackTrace();
+                        }
+                        Address COME_AND_FIND_IT = addresses.get(0);
+                        LatLng latLng = new LatLng(COME_AND_FIND_IT.getLatitude(), COME_AND_FIND_IT.getLongitude());
+                        gMap.addMarker(new MarkerOptions().position(latLng).title(locationOfMyPrecious));
+                        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
                     }
-
-                    @Override
-                    public void onFailure(Call<String> call, Throwable t) {
-                        tvWAYLF.setText("ERROR");
-                    }
-                });
-                new Loading().execute();
-                onMapReady(gMap);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
-
+        mapFragment.getMapAsync(this);
     }
 
-
-    private boolean Check(){
+    private boolean checkMap(){
         GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
         int result = googleAPI.isGooglePlayServicesAvailable(this);
         if(result!=ConnectionResult.SUCCESS){
@@ -99,39 +96,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
 
+    private void createMapView(){
+        try{
+            if(null == gMap){
+                ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMapAsync((OnMapReadyCallback) this);
+                if(null == gMap)
+                    Toast.makeText(getApplicationContext(), "Error creating map", Toast.LENGTH_SHORT).show();
+            }
+        }catch(NullPointerException exception){
+            Log.e("mapApp", exception.toString());
+        }
     }
 
-    private class Loading extends AsyncTask<String, Integer, String> {
-
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-
-        }
-        protected String doInBackground(String... params) {
-            try{
-                if(null == gMap){
-                    ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMapAsync((OnMapReadyCallback) this);
-                    if(null == gMap)
-                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-                }
-            }catch(Exception exception){
-                Log.e("mapApp", exception.toString());
-            }
-            return null;
-        }
-
-        protected void onPostExecute(String s) {
-            MarkerOptions[] markers = new MarkerOptions[places.size()];
-            for(int i = 0; i<places.size(); i++){
-                markers[i] = new MarkerOptions().position(places.get(i));
-                gMap.addMarker(markers[i]);
-
-            }
-            tvWAYLF.setText("We found it");
-        }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        double lat = MARK_LATITUDE;
+        double lng = MARK_LONGTITUDE;
+        LatLng moscow = new LatLng(lat, lng);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(moscow).zoom(10).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        gMap.animateCamera(cameraUpdate);
     }
 }
